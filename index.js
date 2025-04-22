f0bce66cf1ad31ebb3ebe83b15e1c270284dd732
isha-web1 <isha.web1@gmail.com>
2025-04-22 23:40:41 +0600

    user related api create like user get & post

--- index.js
+++ index.js
@@ -28,10 +28,31 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
+    const userCollection = client.db("FlavorHub").collection("users");
     const menuCollection = client.db("FlavorHub").collection("menu");
     const reviewCollection = client.db("FlavorHub").collection("reviews");
     const cartCollection = client.db("FlavorHub").collection("carts");
+
+
+    // user related api
+
+    app.get('/users',  async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      // insert email if user doesnt exists: 
+      // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
     // get menu data
     app.get('/menu', async (req, res) => {
